bb7b72e8aceb50f253456cb708cf8bb8a559f41a
Emir <77904942+ReFo0@users.noreply.github.com>
2022-09-30 20:34:29 +0300

    Update driver.h

--- usermode/driver.h
+++ usermode/driver.h
@@ -3,7 +3,6 @@
 #include <TlHelp32.h>
 
 #define open_process CTL_CODE(FILE_DEVICE_UNKNOWN, 0x186, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
-#define base_adresss CTL_CODE(FILE_DEVICE_UNKNOWN, 0x187, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
 
 struct req {
 	uintptr_t process;
