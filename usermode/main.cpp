3e72ccfeb415de5ba402d82b700eda375a074015
Emir <77904942+ReFo0@users.noreply.github.com>
2022-09-30 20:34:44 +0300

    Update main.cpp

--- usermode/main.cpp
+++ usermode/main.cpp
@@ -7,9 +7,7 @@ int main() {
 	driver = CreateFileA("\\\\.\\refodrv", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
 	auto pid = process_id("notepad.exe");
 	auto base = drv.baseaddress(pid);
-	std::cout << "[?]base_adress : " << base << "\n";
 	auto access = drv.openprocess(pid, PROCESS_ALL_ACCESS); 
-	std::cout << "[?]access mask : " << access << "\n";
 	std::cin.get();
 	return 0;
 }
